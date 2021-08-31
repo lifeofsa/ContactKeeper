235d41e60cba0186ee3411bc007a20bade0216b6
lifeof.sa <abdullahanwar095@gmail.com>
2021-09-01 02:35:18 +0500

    Removed initial route

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.use(express.json({ extended: false }));
 // app.use(express.json({ex}))
 connectDB();
 
-app.get('/', (req, res) =>
-  res.json({ Message: 'Hello Welcome React Developer' }),
-);
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contact', require('./routes/contact'));
 app.use('/api/auth', require('./routes/auth'));
